96c9799d1d1460978a259dc0f61338f56dba075e
bhargavbhegde7 <bhargavbhegde7@gmail.com>
2018-03-10 16:08:08 +0530

    odd even successful

--- odd_even.go
+++ odd_even.go
@@ -4,15 +4,40 @@ import(
   "fmt"
 )
 
+func oddProducer(oddChannel chan int){
+  for i := 1; i<100; i++{
+    if i%2 != 0 {
+      oddChannel<-i
+    }
+  }
+}
+
+func evenProducer(evenChannel chan int){
+  for i := 0; i<100; i++{
+    if i%2 == 0 {
+      evenChannel<-i
+    }
+  }
+}
+
+func consumer(oddChannel chan int, evenChannel chan int){
+  for i := 0; i<100; i++{
+    select{
+      case oddNum := <-oddChannel:
+        fmt.Println("odd  : ",oddNum)
+      case evenNum := <-evenChannel:
+        fmt.Println("even : ",evenNum)
+    }
+  }
+}
+
 func main(){
   oddChannel := make(chan int)
+  evenChannel := make(chan int)
 
-  go func(){
-    i := <-oddChannel
-    fmt.Println("HJELEL", i)
-    }()
-
-  oddChannel <- 42
+  go oddProducer(oddChannel);
+  go evenProducer(evenChannel);
+  go consumer(oddChannel, evenChannel);
 
   var input string
   fmt.Scanln(&input)
